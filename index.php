519aa6d9abcf7974b53a45e00e6529e3cf903509
Josselin-Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-21 22:26:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+#JOB1
+echo("Hello, LaPlateforme!");
+echo "<br>";
+$str="LaPlateforme";
+
+echo $str;
+
+$str2="Vive";
+$str3="!";
+echo"<br>";
+
+echo  $str2 . " " .$str . " " .$str3;
+
+echo"<br>";
+
+$val=6;
+echo $val;
+
+echo"<br>";
+
+$val+=4;
+echo $val;
+
+echo"<br>";
+$myBool=true;
+echo $myBool;
+$myBool=false;
+echo $myBool;
+
+
+#JOB2
+echo"<br>";
+echo  $str2 . " " .$str . " " .$str3; // commentaire en ligne
+
+echo  $str2 . " " .$str . " " .$str3;
+/*commentaire en 
+block*/
+
+#JOB3
+$gagné= true;
+
+$euros= 10;
+
+$centimes= 10.5;
+
+$politesse= "salut";
+
+echo "<table border='1' cellpadding='8' cellspacing='0'>";
+echo "<thead>
+        <tr>
+          <th>Type</th>
+          <th>Nom</th>
+          <th>Valeur</th>
+        </tr>
+      </thead>";
+echo "<tbody>";
+
+
+
+?>
+
+
+
+
