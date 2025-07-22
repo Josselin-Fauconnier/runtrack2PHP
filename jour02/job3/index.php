898529346b684ace3311f15c0d180d7cd64e50c0
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-22 14:26:54 +0200

    Update index.php

--- jour02/job3/index.php
+++ jour02/job3/index.php
@@ -1,1 +1,18 @@
 
+<?php
+$vingtaine = range(0, 20);
+$moitie_50 = range(25, 50);
+$remplacement = "La Plateforme_";
+
+for ($i = 0; $i <= 100; $i++) {
+    if ($i == 42) {
+        echo $remplacement . "<br />";
+    } elseif (in_array($i, $vingtaine)) {
+        echo "<i>" . $i . "</i><br />";
+    } elseif (in_array($i, $moitie_50)) {
+        echo "<u>" . $i . "</u><br />";
+    } else {
+        echo $i . "<br />";
+    }
+}
+?>
