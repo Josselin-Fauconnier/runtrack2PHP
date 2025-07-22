99e32502a44dc01146125792af0b6d7660eb5c6b
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-22 20:49:17 +0200

    Update index.php

--- jour02/job5/index.php
+++ jour02/job5/index.php
@@ -1,1 +1,18 @@
 
+<?php
+function est_premier($nombre_premier) {
+    if ($nombre_premier <= 1) {
+        return false;
+    }
+    
+    for ($i = 2; $i <= sqrt($nombre_premier); $i++) {
+        if ($nombre_premier % $i == 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+
+?>
