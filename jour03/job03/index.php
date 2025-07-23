56029847ffc023fd18e285754a37c8a354e8fd11
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-23 15:49:02 +0200

    Update index.php

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,1 +1,14 @@
+<?php
+$str = "I'm sorry Dave I'm afraid I can't do that";
 
+$voyelles = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+for ($i = 0; $i < strlen($str); $i++) {
+    $lettres= strtolower($str[$i]); 
+
+    
+    if (in_array($lettres, $voyelles)) {
+        echo $str[$i]; 
+    }
+}
+?>
