a4a062260c66acbc5ab2c89aae284490786db79c
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-23 10:22:56 +0200

    Update index.php

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -1,1 +1,9 @@
-
+<?php
+$pairs_impairs= array(200, 204, 173, 98, 171, 404, 459.);
+foreach ($pairs_impairs as $pair_impair)
+       if ($pair_impair %2==0){
+        echo"$pair_impair est pair <br />";
+       } else {
+    echo "$pair_impair est impair <br />";
+}
+?>
