12de34a6548bc7aefa736b839eb8e98062f87870
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-24 22:13:06 +0200

    Update index.php

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -1,1 +1,9 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
 
+$str = "Les choses que l'on possèdent finissent par nous posséder";
+
+$str_sans_accents = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+
+echo strrev($str_sans_accents);
+?>
