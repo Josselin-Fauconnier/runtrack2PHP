aed31807f7097a7ef3fea0e0a4931c21602222b1
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-25 09:46:10 +0200

    Update index.php

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -1,1 +1,16 @@
+<?php
+
+$str = "Certaines choses changent, et d'autres ne changeront jamais."; 
+
+$premiere_lettre = $str[0];
+$reste_chaine = substr($str, 1);
+$str_rotation = $reste_chaine . $premiere_lettre;
+$str_rotation = ucfirst($str_rotation);
+$derniere_lettre = substr($str_rotation, -1);
+$derniere_lettre_minuscule = strtolower($derniere_lettre);
+$str_rotation[strlen($str_rotation) - 1] = $derniere_lettre_minuscule;
+
+echo $str_rotation;
+
+?>
 
