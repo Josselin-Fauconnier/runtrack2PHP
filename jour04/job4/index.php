2bba4764871c15777505dc31917518e33620766f
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-25 15:56:46 +0200

    Update index.php

--- jour04/job4/index.php
+++ jour04/job4/index.php
@@ -1,1 +1,23 @@
 
+<?php
+echo'<form method="POST" action="">
+  <input type="text" name="prenom" placeholder="Prénom">
+  <input type="text" name="nom" placeholder="Nom">
+  <input type="submit" value="Envoyer">
+</form>
+';
+
+if (!empty($_POST)) {
+    echo "<table border='1'>";
+    echo "<tr><th>Argument</th><th>Valeur</th></tr>";
+
+    foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>$key</td><td>$value</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
+
+?>
