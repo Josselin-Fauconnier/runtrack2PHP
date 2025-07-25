b9d62d6d01b7deb1f511edb25f834644fd4ebafe
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-25 14:18:52 +0200

    Update index.php

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -1,1 +1,12 @@
+<?php
+echo'<form method="POST" action="">
+  <input type="text" name="prenom">
+  <input type="text" name="nom">
+  <input type="submit" value="Envoyer">
+</form>';
 
+if (!empty($_POST)) {
+    echo "Le nombre d'arguments POST envoyes est : " . count($_POST);
+}
+
+?>
