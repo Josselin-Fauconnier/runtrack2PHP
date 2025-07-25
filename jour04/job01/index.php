c52b5509c9532d92cca14d67e5e2600eaf67bc93
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-25 11:14:19 +0200

    Update index.php

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -1,1 +1,13 @@
 
+<?php
+
+echo "<form method=\"GET\" action=\"\">
+  <input type=\"text\" name=\"prenom\">
+  <input type=\"text\" name=\"nom\">
+  <input type=\"submit\" value=\"Envoyer\">
+</form>";
+
+if (isset($_GET) && !empty($_GET)) {
+    echo "Le nombre d'arguments GET envoyés est : " . count($_GET);
+}
+?>
