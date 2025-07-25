7c51c2474448e7dd65c6f5980436b6d093c42f4a
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-25 11:32:59 +0200

    Update index.php

--- jour04/job2/index.php
+++ jour04/job2/index.php
@@ -1,1 +1,23 @@
 
+<?php
+
+echo '<form method="GET" action="">
+  <input type="text" name="prenom" placeholder="Prénom">
+  <input type="text" name="nom" placeholder="Nom">
+  <input type="submit" value="Envoyer">
+</form>';
+
+
+if (!empty($_GET)) {
+    echo "<table border='1'>";
+    echo "<tr><th>Argument</th><th>Valeur</th></tr>";
+
+    foreach ($_GET as $key => $value) {
+        echo "<tr>";
+        echo "<td>$key</td><td>$value</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
+?>
