3d08e1a6c715c1f587e15e97d98513cc4f4f7ee3
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-27 08:57:01 +0200

    Update index.php

--- jour04/job5/index.php
+++ jour04/job5/index.php
@@ -1,1 +1,17 @@
 
+<?php
+echo '<form method="POST" action="">
+  <input type="text" name="username" placeholder="Nom d\'utilisateur">
+  <input type="password" name="password" placeholder="Mot de passe">
+  <input type="submit" value="Connexion">
+</form>
+';
+
+if (!empty($_POST)) {
+    if ($_POST['username'] === 'John' && $_POST['password'] === 'Rambo') {
+        echo "C’est pas ma guerre";
+    } else {
+        echo "Votre pire cauchemar";
+    }
+}
+?>
