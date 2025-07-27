8a3b9f7118e6f146a1af3836137e356e720f3309
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-27 15:09:26 +0200

    Update index.php

--- jour04/job6/index.php
+++ jour04/job6/index.php
@@ -1,1 +1,22 @@
 
+<?php
+echo '<form method="GET" action="">
+  <input type="text" name="nombre" placeholder="Entrez un nombre">
+  <input type="submit" value="Vérifier">
+</form>
+';
+
+if (!empty($_GET['nombre'])) {
+    $nombre = $_GET['nombre'];
+
+    if (is_numeric($nombre)) {
+        if ($nombre % 2 == 0) {
+            echo "Nombre pair";
+        } else {
+            echo "Nombre impair";
+        }
+    } else {
+        echo "Veuillez entrer un nombre valide.";
+    }
+}
+?>
