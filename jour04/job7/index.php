cdbd453037266c3123a11148b591d655990f90f1
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-27 16:31:43 +0200

    Update index.php

--- jour04/job7/index.php
+++ jour04/job7/index.php
@@ -1,1 +1,10 @@
 
+<?php
+echo '<form method="GET" action="">
+  <input type="text" name="largeur" placeholder="Largeur ">
+  <input type="text" name="hauteur" placeholder="Hauteur">
+  <input type="submit" >
+</form>';
+
+        //j'arrive pas à faire la maison et j'ai pas envie de rendre un truc par ia que je comprends pas  
+?>
