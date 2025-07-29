df54da0d85c73f5e18bbb62f9b966c2a99b2fcfc
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-29 16:10:29 +0200

    Update index.php

--- jour7/job4/index.php
+++ jour7/job4/index.php
@@ -1,1 +1,34 @@
 
+<?php
+
+function calcule($a=70, $operation="+", $b=13) {
+    if ($operation == "+") {
+        return $a + $b;
+    } elseif ($operation == "-") {
+        return $a - $b;
+    } elseif ($operation == "*") {
+        return $a * $b;
+    } elseif ($operation == "/") {
+        
+        if ($b != 0) {
+            return $a / $b;
+        } else {
+            return "division par zéro";
+        }
+    } elseif ($operation == "%") {
+       
+        if ($b != 0) {
+            return $a % $b;
+        } else {
+            return "division par zéro";
+        }
+    } else {
+        return "Opération inconnue";
+    }
+}
+
+
+echo calcule();
+
+
+?>
