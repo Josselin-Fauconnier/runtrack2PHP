f422d33684abb1baf210c467dce58fb898d65d52
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-29 21:53:23 +0200

    Update index.php

--- jour04/job7/index.php
+++ jour04/job7/index.php
@@ -5,6 +5,26 @@ echo '<form method="GET" action="">
   <input type="text" name="hauteur" placeholder="Hauteur">
   <input type="submit" >
 </form>';
+if (!empty($_GET)) {
+    $largeur = (int)$_GET['largeur'];
+    $hauteur = (int)$_GET['hauteur'];
 
-        //j'arrive pas à faire la maison et j'ai pas envie de rendre un truc par ia que je comprends pas  
+    if ($largeur >= 2 && $hauteur > 0) {
+        $toit = (int)($largeur / 2);
+        echo '<pre>';
+        for ($i = 0; $i < $toit; $i++) {
+            echo str_repeat(' ', $toit - $i);
+            echo '/';
+            echo str_repeat(' ', 2 * $i);
+            echo "\\\n";
+        }
+        echo '/'.str_repeat('_', $largeur).'\\'."\n";
+        for ($j = 0; $j < $hauteur; $j++) {
+            echo '|'.str_repeat(' ', $largeur).'|'."\n";
+        }
+        echo '</pre>';
+    } else {
+        echo 'Valeurs invalides';
+    }
+       
 ?>
