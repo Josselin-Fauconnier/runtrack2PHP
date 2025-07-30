09caf16e396dcfa679e5e9f5ce425b7f101c56e5
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-30 16:18:52 +0200

    Update index.php

--- jour7/job5/index.php
+++ jour7/job5/index.php
@@ -1,1 +1,16 @@
 
+<?php
+
+function occurrences($str, $char) {
+    $compte = 0;
+
+    // Parcours de la chaîne caractère par caractère
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($str[$i] === $char) {
+            $compte++;
+        }
+    }
+
+    // Retour du résultat
+    return $compte;
+}
