432f1857b9a9939ec14c4678ae93cb9706cd8954
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-30 12:40:15 +0200

    Update index.php

--- jour04/job7/index.php
+++ jour04/job7/index.php
@@ -3,28 +3,29 @@
 echo '<form method="GET" action="">
   <input type="text" name="largeur" placeholder="Largeur ">
   <input type="text" name="hauteur" placeholder="Hauteur">
-  <input type="submit" >
+  <input type="submit">
 </form>';
+
 if (!empty($_GET)) {
     $largeur = $_GET['largeur'];
     $hauteur = $_GET['hauteur'];
 
     if ($largeur >= 2 && $hauteur > 0) {
-        $toit = ($largeur / 2);
-        echo '<pre>';
+        echo '<pre>'; // Ajout de la balise pre pour le formatage
+        $toit = floor($largeur / 2); 
         for ($i = 0; $i < $toit; $i++) {
             echo str_repeat(' ', $toit - $i);
             echo '/';
             echo str_repeat(' ', 2 * $i);
-            echo "\\\n";
+            echo "\\\n"; // Un seul backslash suffit ici
         }
-        echo '/'.str_repeat('_', $largeur).'\\'."\n";
+        echo '/'.str_repeat('_', $largeur)."\\\n";
         for ($j = 0; $j < $hauteur; $j++) {
-            echo '|'.str_repeat(' ', $largeur).'|'."\n";
+            echo '|'.str_repeat(' ', $largeur)."|\n";
         }
         echo '</pre>';
     } else {
         echo 'Valeurs invalides';
     }
-       
+}
 ?>
