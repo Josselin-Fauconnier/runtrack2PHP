2ed327581953c8cbbad6947f7adb73fff829c164
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-31 15:01:09 +0200

    Update index.php

--- jour8/job3/index.php
+++ jour8/job3/index.php
@@ -1,1 +1,46 @@
 
+<?php
+session_start(); 
+
+if (isset($_GET['reset'])) {
+    unset($_SESSION['prenoms']);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!empty($_POST['prenom'])) {
+        $prenom = $_POST['prenom']; 
+
+        
+        if (!isset($_SESSION['prenoms'])) {
+            $_SESSION['prenoms'] = [];
+        }
+
+        
+        $_SESSION['prenoms'][] = $prenom;
+    }
+}
+
+
+if (!empty($_SESSION['prenoms'])) {
+    echo "<ul>";
+    foreach ($_SESSION['prenoms'] as $p) {
+        echo "<li>$p</li>";
+    }
+    echo "</ul>";
+} 
+
+echo '
+<form method="post">
+    <label for="prenom">Entrez un prénom :</label>
+    <input type="text" name="prenom" id="prenom">
+    <button type="submit">Valider</button>
+</form>
+
+<form method="get">
+    <button type="submit" name="reset" value="1">Reset</button>
+</form>
+';
+?>
+
+
+
