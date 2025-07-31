9b537743947f6e50273a18ec3e3be755f54e6198
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-31 14:40:42 +0200

    Update index.php

--- jour8/job2/index.php
+++ jour8/job2/index.php
@@ -1,1 +1,26 @@
 
+<?php
+
+if (isset($_GET['reset'])) {
+    
+    setcookie('nbvisites', '', time() - 7400);
+    $_COOKIE['nbvisites'] = 0; 
+}
+
+
+if (isset($_COOKIE['nbvisites'])) {
+    $nbvisites = $_COOKIE['nbvisites'] + 1;
+} else {
+    $nbvisites = 1; 
+}
+
+setcookie('nbvisites', $nbvisites, time() + 7400);
+
+
+echo "<p>Nombre de visites : $nbvisites</p>";
+
+
+echo '<form method="get">
+        <button type="submit" name="reset" value="1">Reset</button>
+      </form>';
+?>
