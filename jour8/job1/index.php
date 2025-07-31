6f533bc56ce210c6d8a857c95891808426817b38
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-31 14:19:01 +0200

    Update index.php

--- jour8/job1/index.php
+++ jour8/job1/index.php
@@ -1,1 +1,23 @@
+<?php
+session_start(); 
 
+if (isset($_GET['reset'])) {
+    $_SESSION['nbvisites'] = 0; 
+}
+
+
+if (isset($_SESSION['nbvisites'])) {
+    $_SESSION['nbvisites']++; 
+} else {
+    $_SESSION['nbvisites'] = 1; 
+}
+
+
+echo "<p>Nombre de visites : " . $_SESSION['nbvisites'] . "</p>";
+
+
+echo '<form method="get">
+        <button type="submit" name="reset" value="1">Reset</button>
+      </form>';
+
+?>
