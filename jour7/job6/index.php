9b56c6634f5c05aaabd9307b01af4a94e9a86c03
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-07-31 09:44:25 +0200

    Update index.php

--- jour7/job6/index.php
+++ jour7/job6/index.php
@@ -1,1 +1,17 @@
+<?php
+function leetSpeak($str) {
+    $leetCode = [
+        'a' => '4',
+        'b' => '8',
+        'e' => '3',
+        'g' => '6',
+        'l' => '1',
+        's' => '5',
+        't' => '7'
+    ];
 
+    $minusStr = strtolower($str);
+    $leetStr = strtr($minusStr, $leetCode);
+    return $leetStr;
+}
+?>
