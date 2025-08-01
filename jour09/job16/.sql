dbc4e8aa3f35a91eff9cf9566ab3fdbd6fac5bb0
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-08-01 14:55:59 +0200

    Update .sql

--- jour09/job16/.sql
+++ jour09/job16/.sql
@@ -1,1 +1,3 @@
-
+SELECT  etages.nom AS nom_etage,salles.nom AS 'Biggest Room',salles.capaciteFROM salles
+JOIN etages ON salles.etage = etages.numeroORDER BY CAST(salles.capacite AS SIGNED) DESC
+LIMIT 1;
