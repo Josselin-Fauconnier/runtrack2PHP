db1681b3209aa509c40becc05655f7d2f7b0d65b
Josselin.Fauconnier <josselin.fauconnier@laplateforme.io>
2025-08-02 17:59:11 +0200

    Update index.php

--- jour10/job08/index.php
+++ jour10/job08/index.php
@@ -1,1 +1,36 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "root", "job2");
+
+if ($mysqli->connect_error) {
+    exit("Échec de la connexion : " . $mysqli->connect_error);
+}
+
+$query = "SELECT SUM(capacite) AS capacite_totale FROM salles";
+$result = $mysqli->query($query);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<thead><tr>";
+
+    while ($fieldinfo = $result->fetch_field()) {
+        echo "<th>" . htmlspecialchars($fieldinfo->name) . "</th>";
+    }
+
+    echo "</tr></thead><tbody>";
+
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        foreach ($row as $value) {
+            echo "<td>" . htmlspecialchars($value) . "</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</tbody></table>";
+} else {
+    echo "Aucune donnée.";
+}
+
+$mysqli->close();
+?>
 
